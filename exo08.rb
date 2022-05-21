78021cdf98fb8c40152f71a9cd5657a144f4d5c4
Samia3 <samiaamrouche94@gmail.com>
2022-05-21 10:15:34 +0200

    puissance

---
+++ exo08.rb
@@ -0,0 +1,26 @@
+def argument
+
+	saisie = /\A[-+]?\d+\z/
+
+	if(ARGV.length != 2)
+		puts "error"
+	elsif(ARGV[0].match?(saisie) == false || ARGV[1].match?(saisie)== false)
+		puts "error"
+	else
+		puissance
+	end
+end
+
+def puissance
+	var = 1
+
+	for i in 1..(ARGV[1].to_i)
+
+		var = var * ARGV[0].to_i
+		
+	end
+	puts var
+end
+
+
+argument
