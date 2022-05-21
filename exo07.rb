8595f713339b01ee094280110c1c6d6622509537
Samia3 <samiaamrouche94@gmail.com>
2022-05-21 09:51:36 +0200

    taille d'une chaine

---
+++ exo07.rb
@@ -0,0 +1,18 @@
+def argument
+
+	saisie = /\A[-+]?\D+\z/
+
+	if(ARGV.length != 1)
+		puts "error"
+	elsif(ARGV[0].match?(saisie) == false)
+		puts "error"
+	else
+		taille
+	end
+end
+
+def taille
+	puts ARGV[0].length
+end
+
+argument
